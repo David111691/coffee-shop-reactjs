1b71111c3f00f148c30e463c02d66d44d4f2cacd
david111691 <davidchaikovsky100@gmail.com>
2025-06-03 16:16:23 +0300

    2nd commit

---
+++ src/Components/AnnouncementBar.tsx
@@ -0,0 +1,9 @@
+function AnnouncementBar() {
+	return (
+		<div className="announcement-bar">
+			<span className="announcement-bar__label">Free shipping on orders over $100</span>
+		</div>
+	);
+}
+
+export default AnnouncementBar;
