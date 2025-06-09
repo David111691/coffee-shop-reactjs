e7780ae73f447f339ae644b1df49b471b8daff62
david111691 <davidchaikovsky100@gmail.com>
2025-06-09 21:48:40 +0300

    some tweaks

--- src/Components/Cart.tsx
+++ src/Components/Cart.tsx
@@ -1,5 +1,5 @@
 import { createPortal } from "react-dom";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
 const Cart: React.FC = ({ handleClick, isClosing }) => {
   useEffect(() => {
@@ -15,11 +15,26 @@ const Cart: React.FC = ({ handleClick, isClosing }) => {
       <div className={`cart ${isClosing ? "cart--slide-out" : ""}`}>
         <div className="cart__header">
           <p>Cart</p>
-          <button className="cart__close-button" onClick={handleClick}>&times;</button>
+          <button
+            className="cart__close-button"
+            onClick={handleClick}
+            disabled={isClosing}
+          >
+            &times;
+          </button>
         </div>
-        <div className="cart__empty-message"><p>Your cart is empty</p></div>
+        <div className="cart__empty-message">
+          <p>Your cart is empty</p>
+        </div>
       </div>
-      {createPortal(<div className="cart__overlay-background"></div>, document.body)}
+      {createPortal(
+        <div
+          className={`cart__overlay-background ${
+            isClosing ? "cart__overlay--appear-out" : ""
+          }`}
+        ></div>,
+        document.body
+      )}
     </>
   );
 };
